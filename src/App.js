4dd4d035a7e2b73c1f8851f8091ca300adb232bc
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-10 15:38:28 -0500

    Add assignent from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { peopleData } from "./Data";
+const TableRow = ({
+    firstName,
+    lastName,
+    age,
+    profession,
+    yearlySalary,
+    yearlyTotalExpenses,
+    creditScore,
+    yearlySaving,
+    averageMonthlyExpense
+}) => {
+    return (
+        <tr>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+            <td>{profession}</td>
+            <td>{age}</td>
+            <td>{yearlySalary}</td>
+            <td>{yearlyTotalExpenses}</td>
+            <td>{creditScore}</td>
+            <td>{yearlySaving}</td>
+            <td>{averageMonthlyExpense}</td>
+        </tr>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                    <th>Profession</th>
+                    <th>Yearly Salary</th>
+                    <th>Yearly Expenses</th>
+                    <th>Credit Score</th>
+                    <th>Yearly Saving</th>
+                    <th>Average Monthly Expense</th>
+                </tr>
+            </thead>
+            <tbody>
+                {peopleData.map((person, idx) => {
+                    return (
+                        <TableRow
+                            key={idx}
+                            firstName={person.firstName}
+                            lastName={person.lastName}
+                            age={person.age}
+                            profession={person.profession}
+                            yearlySalary={person.finance.yearlySalary}
+                            yearlyTotalExpenses={
+                                person.finance.yearlyTotalExpenses
+                            }
+                            creditScore={person.finance.creditScore}
+                            yearlySaving={
+                                person.finance.yearlySalary -
+                                person.finance.yearlyTotalExpenses
+                            }
+                            averageMonthlyExpense={
+                                person.finance.yearlyTotalExpenses / 12
+                            }
+                        />
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,321 @@
+export const peopleData = [
+    {
+        firstName: "Matthew",
+        lastName: "Clark",
+        age: 59,
+        profession: "Barrister's clerk",
+        finance: {
+            yearlySalary: 55069.77,
+            yearlyTotalExpenses: 29435.91,
+            creditScore: 343
+        }
+    },
+    {
+        firstName: "Ana",
+        lastName: "Walker",
+        age: 24,
+        profession: "Horticultural consultant",
+        finance: {
+            yearlySalary: 71107.59,
+            yearlyTotalExpenses: 36835.42,
+            creditScore: 393
+        }
+    },
+    {
+        firstName: "Joshua",
+        lastName: "Lowe",
+        age: 51,
+        profession: "Interior and spatial designer",
+        finance: {
+            yearlySalary: 146967.3,
+            yearlyTotalExpenses: 66695.13,
+            creditScore: 300
+        }
+    },
+    {
+        firstName: "Kimberly",
+        lastName: "Armstrong",
+        age: 33,
+        profession: "Analytical chemist",
+        finance: {
+            yearlySalary: 83560.31,
+            yearlyTotalExpenses: 101715.72,
+            creditScore: 444
+        }
+    },
+    {
+        firstName: "James",
+        lastName: "Gordon",
+        age: 52,
+        profession: "Optician, dispensing",
+        finance: {
+            yearlySalary: 35050.55,
+            yearlyTotalExpenses: 55355.87,
+            creditScore: 514
+        }
+    },
+    {
+        firstName: "Larry",
+        lastName: "Powell",
+        age: 26,
+        profession: "Media planner",
+        finance: {
+            yearlySalary: 34832.67,
+            yearlyTotalExpenses: 75580.31,
+            creditScore: 447
+        }
+    },
+    {
+        firstName: "Jared",
+        lastName: "Hill",
+        age: 33,
+        profession: "Surveyor, building",
+        finance: {
+            yearlySalary: 115860.78,
+            yearlyTotalExpenses: 21148.46,
+            creditScore: 686
+        }
+    },
+    {
+        firstName: "Joseph",
+        lastName: "Olsen",
+        age: 26,
+        profession: "Therapist, speech and language",
+        finance: {
+            yearlySalary: 35091.23,
+            yearlyTotalExpenses: 33651.81,
+            creditScore: 305
+        }
+    },
+    {
+        firstName: "Dylan",
+        lastName: "Bass",
+        age: 34,
+        profession: "Economist",
+        finance: {
+            yearlySalary: 120124.72,
+            yearlyTotalExpenses: 109333.69,
+            creditScore: 650
+        }
+    },
+    {
+        firstName: "Dorothy",
+        lastName: "Young",
+        age: 37,
+        profession: "Financial risk analyst",
+        finance: {
+            yearlySalary: 117321.04,
+            yearlyTotalExpenses: 107925.14,
+            creditScore: 712
+        }
+    },
+    {
+        firstName: "Angela",
+        lastName: "Schneider",
+        age: 54,
+        profession: "Risk manager",
+        finance: {
+            yearlySalary: 48411.38,
+            yearlyTotalExpenses: 39837.93,
+            creditScore: 502
+        }
+    },
+    {
+        firstName: "Gloria",
+        lastName: "Lucero",
+        age: 36,
+        profession: "Physiological scientist",
+        finance: {
+            yearlySalary: 91882.85,
+            yearlyTotalExpenses: 45710.92,
+            creditScore: 702
+        }
+    },
+    {
+        firstName: "Adam",
+        lastName: "Cruz",
+        age: 26,
+        profession: "Materials engineer",
+        finance: {
+            yearlySalary: 87350.53,
+            yearlyTotalExpenses: 66431.14,
+            creditScore: 463
+        }
+    },
+    {
+        firstName: "Tyler",
+        lastName: "Preston",
+        age: 40,
+        profession: "Animator",
+        finance: {
+            yearlySalary: 137511.63,
+            yearlyTotalExpenses: 105641.28,
+            creditScore: 775
+        }
+    },
+    {
+        firstName: "Gina",
+        lastName: "Bush",
+        age: 31,
+        profession: "Drilling engineer",
+        finance: {
+            yearlySalary: 146860.49,
+            yearlyTotalExpenses: 85919.45,
+            creditScore: 499
+        }
+    },
+    {
+        firstName: "Erica",
+        lastName: "Yang",
+        age: 57,
+        profession: "Education officer, environmental",
+        finance: {
+            yearlySalary: 148256.61,
+            yearlyTotalExpenses: 56071.4,
+            creditScore: 629
+        }
+    },
+    {
+        firstName: "Haley",
+        lastName: "Martinez",
+        age: 33,
+        profession: "Chief of Staff",
+        finance: {
+            yearlySalary: 45377.06,
+            yearlyTotalExpenses: 70132.78,
+            creditScore: 837
+        }
+    },
+    {
+        firstName: "Jill",
+        lastName: "Huff",
+        age: 43,
+        profession: "Psychotherapist, child",
+        finance: {
+            yearlySalary: 123787.34,
+            yearlyTotalExpenses: 109347.42,
+            creditScore: 668
+        }
+    },
+    {
+        firstName: "Beth",
+        lastName: "Miller",
+        age: 18,
+        profession: "Teaching laboratory technician",
+        finance: {
+            yearlySalary: 148221.11,
+            yearlyTotalExpenses: 73162.78,
+            creditScore: 746
+        }
+    },
+    {
+        firstName: "William",
+        lastName: "Harris",
+        age: 69,
+        profession: "Merchandiser, retail",
+        finance: {
+            yearlySalary: 98475.4,
+            yearlyTotalExpenses: 106543.95,
+            creditScore: 477
+        }
+    },
+    {
+        firstName: "Monique",
+        lastName: "Vazquez",
+        age: 42,
+        profession: "Market researcher",
+        finance: {
+            yearlySalary: 44834.52,
+            yearlyTotalExpenses: 30932.22,
+            creditScore: 362
+        }
+    },
+    {
+        firstName: "Brian",
+        lastName: "Lee",
+        age: 70,
+        profession: "Newspaper journalist",
+        finance: {
+            yearlySalary: 138577.75,
+            yearlyTotalExpenses: 43186.48,
+            creditScore: 444
+        }
+    },
+    {
+        firstName: "Craig",
+        lastName: "Donaldson",
+        age: 64,
+        profession: "Insurance account manager",
+        finance: {
+            yearlySalary: 43108.82,
+            yearlyTotalExpenses: 28604.09,
+            creditScore: 457
+        }
+    },
+    {
+        firstName: "Cody",
+        lastName: "Fields",
+        age: 40,
+        profession: "Education officer, museum",
+        finance: {
+            yearlySalary: 31114.29,
+            yearlyTotalExpenses: 102576.35,
+            creditScore: 356
+        }
+    },
+    {
+        firstName: "Anthony",
+        lastName: "Kelly",
+        age: 35,
+        profession: "Programmer, multimedia",
+        finance: {
+            yearlySalary: 104167.36,
+            yearlyTotalExpenses: 98374.35,
+            creditScore: 392
+        }
+    },
+    {
+        firstName: "Patty",
+        lastName: "King",
+        age: 48,
+        profession: "Risk manager",
+        finance: {
+            yearlySalary: 136761.59,
+            yearlyTotalExpenses: 107969.31,
+            creditScore: 301
+        }
+    },
+    {
+        firstName: "Rodney",
+        lastName: "Conway",
+        age: 28,
+        profession: "Planning and development surveyor",
+        finance: {
+            yearlySalary: 30935.09,
+            yearlyTotalExpenses: 38233.13,
+            creditScore: 692
+        }
+    },
+    {
+        firstName: "Susan",
+        lastName: "Benitez",
+        age: 32,
+        profession: "Musician",
+        finance: {
+            yearlySalary: 48412.65,
+            yearlyTotalExpenses: 76107.23,
+            creditScore: 549
+        }
+    },
+    {
+        firstName: "Linda",
+        lastName: "Washington",
+        age: 41,
+        profession: "Accountant, chartered certified",
+        finance: {
+            yearlySalary: 94578.36,
+            yearlyTotalExpenses: 86779.57,
+            creditScore: 756
+        }
+    }
+];
